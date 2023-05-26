0b965aab36911c7c25cd4e7044be6fa43ebd38a5
corcasta <corcasta@outlook.com>
2023-05-26 19:16:45 +0200

    Cleaning files

---
+++ camera/calibrate.py
@@ -0,0 +1,84 @@
+import os
+import sys
+import cv2
+import argparse
+import numpy as np
+import pandas as pd
+from glob import glob
+
+
+def calibrate(pattern_size, visualize=False):
+    """
+    pattern_size: A tuple of integers representing the number of inner 
+                  corners in chessboard.
+                  Ex: (vertical corners, horizontal corners)
+                  
+    visualize: Boolean, displays chessboard patterns during calibration.
+    """
+    images = sorted(glob(os.path.join(os.getcwd(), 
+                                      "pictures", 
+                                      "*")))
+    assert images
+    # prepare object points, like (0,0,0), (1,0,0), (2,0,0) ....,(6,5,0)
+    objp = np.zeros((pattern_size[0]*pattern_size[1],3), np.float32)
+    objp[:,:2] = np.mgrid[0:pattern_size[0],0:pattern_size[1]].T.reshape(-1,2)
+
+    # Arrays to store object points and image points from all the images.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
+
+    for fname in images:
+        img = cv2.imread(fname)
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        ret, corners = cv2.findChessboardCorners(gray, pattern_size)
+        # If found, add object points, image points (after refining them)
+        if ret == True:
+            objpoints.append(objp)
+            imgpoints.append(corners)
+            # Draw and display the corners
+            if visualize:
+                img = cv2.drawChessboardCorners(img, pattern_size, corners,ret)
+                cv2.imshow('img',img)
+                cv2.waitKey(500)
+    cv2.destroyAllWindows()
+
+    ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1], None, None)
+    img = cv2.imread(images[0])
+    h,  w = img.shape[:2]
+    newcameramtx, roi = cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
+    
+    
+    #Saves camera intrinsics and distortion coefficients
+    mtx_df = pd.DataFrame(mtx)
+    mtx_df.to_string("details/intrinsic_matrix.txt", index=False, header=False)
+
+    #data = pd.DataFrame(newcameramtx)
+    #data.to_string("newcam_intrinsic_matrix.txt", index=False, header=False)
+    
+    dist_df = pd.DataFrame(dist)
+    dist_df.to_string("details/dist_coefficients.txt", index=False, header=False)
+    
+
+def main(args):
+    if args.corners == None:
+        print(("ERROR: Please provide number of vertical " 
+               "and horizontal corners with -c argument: e.g. -c 7 7"))
+        sys.exit()
+    elif len(args.corners) == 2:
+        pattern_size = tuple(args.corners)
+        calibrate(pattern_size)
+    else:
+        print("ERROR: Invalid input provided. Please provide just 2 integers")
+        sys.exit()
+    
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--corners", 
+                        help=("Provide only 2 integers representing the number of " 
+                              "inner corners in chessboard (vertical corners, horizontal corners)"),
+                        nargs='+', 
+                        type=int)
+    args = parser.parse_args()
+    main(args)
+    
---
+++ model/benchmark.py
@@ -0,0 +1,78 @@
+import os
+import cv2
+import time
+import imutils
+from glob import glob
+from pathlib import Path
+import supervision as sv
+from ultralytics import YOLO
+import matplotlib.pyplot as plt
+
+       
+def main():
+    # Select model to benchmark
+    weights = os.getcwd() + "/runs/detect" + "/train4/weights/best.pt"
+    # SElect video to benchmark
+    video = str(Path(os.getcwd()).parent) + "/videos/" + "sail_amsterdam.mp4"
+    
+    #*********************FPS-DISPLAY-SETTINGS***********************
+    # used to record the time when we processed last frame
+    prev_frame_time = 0
+    # used to record the time at which we processed current frame
+    new_frame_time = 0
+    # font which we will be using to display FPS
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    #*********************FPS-DISPLAY-SETTINGS***********************
+    
+    #Model Instance
+    model = YOLO(weights)
+
+    box_annotator = sv.BoxAnnotator(
+        thickness=1,
+        text_thickness=1,
+        text_scale=0.5,
+        text_padding=5
+    )
+    
+    for result in model.track(video, show=False, stream=True):
+        frame = result.orig_img
+        detections = sv.Detections.from_yolov8(result)
+        
+        if result.boxes.id is not None:
+            detections.tracker_id = result.boxes.id.cpu().numpy().astype(int) 
+        
+        print(detections)
+        labels = [
+            f"#:{tracker_id}, {model.model.names[class_id]}: {confidence:0.2f}"
+            for xyxy, confidence, class_id, tracker_id 
+            in detections
+        ]
+        
+        frame = box_annotator.annotate(scene=frame, detections=detections, labels=labels)
+        
+        # time when we finish processing for this frame
+        new_frame_time = time.time()
+        # fps will be number of frame processed in given time frame
+        # since their will be most of time error of 0.001 second
+        # we will be subtracting it to get more accurate result
+        fps = 1/(new_frame_time-prev_frame_time)
+        prev_frame_time = new_frame_time
+        # converting the fps into integer
+        fps = int(fps)
+        # converting the fps to string so that we can display it on frame
+        # by using putText function
+        fps = str(fps)
+        
+        # putting the FPS count on the frame
+        cv2.putText(frame, "FPS: {}".format(fps), (7, 30), font, 1, (100, 255, 0), 3, cv2.LINE_AA)
+        
+        cv2.imshow("Boats-Detection", imutils.resize(frame, width=1280))
+        if (cv2.waitKey(20) == ord("q")):
+            break
+    
+    
+if __name__ == "__main__":
+    # Setting the starting path of the script
+    script_path = os.path.realpath(__file__)
+    os.chdir(Path(script_path).parent)
+    main()
